a875875ed6a21fb45e613049f9ebc25990527387
Abdurahman Haji Abdi <abdulrahmanxambali@gmail.com>
2025-11-18 18:15:45 +0100

    Feature: XUnit Dependency Injection for Password Fields (Server Only at the moment. WIP

---
+++ server/api/Services/PasswordService.cs
@@ -0,0 +1,16 @@
+﻿namespace api.Services;
+
+public class PasswordService : IPasswordService
+{
+    // placeholder service
+    
+    public string HashPassword(string password)
+    {
+        return $"HASHED:{password}";
+    }
+
+    public bool VerifyPassword(string password, string hash)
+    {
+        return hash == $"HASHED:{password}";
+    }
+}
